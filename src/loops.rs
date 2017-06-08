0c963bf80234985e17fc6daac834c2d2738cf534
Crandel <cradlemann@gmail.com>
2017-06-08 08:29:20 +0300

    Add some loops

--- src/loops.rs
+++ src/loops.rs
@@ -1,12 +1,25 @@
 pub fn loops() {
+
     let mut x = 1;
     loop {
         if ((x % 2) == 0) {
-            println!("{}", x);
+            println!("infinite loop: x is {}", x);
         }
         if x == 15 {
             break;
         }
-        x += 1
+        x += 1;
+    }
+    println!("{}", "7".repeat(5));
+
+    let mut y = 1;
+    while y <= 15 {
+        println!("while: y is {}", y);
+        y += 1;
+    }
+    println!("{}", "c".repeat(5));
+
+    for z in 1..10 {
+        println!("For: z is {}", z);
     }
 }
