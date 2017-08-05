e449c8f028815c35b4b96fe2f3f5a3b0c119b709
Crandel <cradlemann@gmail.com>
2017-08-05 23:40:59 +0300

    add some utf8 strings

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@
 extern crate test_lib;
 
 use test_lib::prim::{primitives, conditionals};
-use test_lib::{structs, enums, sequences};
+use test_lib::*;
 
 fn main() {
     println!("{}", "+".repeat(10));
-    primitives::primitives_test();
+    // primitives::primitives_test();
     // println!("{}", "-".repeat(10));
-    conditionals::conditionals_test();
+    // conditionals::conditionals_test();
     // println!("{}", "*".repeat(10));
     // loops::loops_test();
-    // strings::strings_test();
+    strings::strings_test();
     // input::input_test();
-    sequences::seq_test();
+    // sequences::seq_test();
     // funcs::func_test();
-    structs::structs_test();
-    enums::enums_test();
+    // structs::structs_test();
+    // enums::enums_test();
     println!("{}", "/".repeat(10));
 }
--- src/strings.rs
+++ src/strings.rs
@@ -1,19 +1,19 @@
 pub fn strings_test() {
-    let strng = "Some simple string in the world";
+    let strng = "Привіт Some simple string in the world";
     println!("Lenght: {}", strng.len());
     // Split using an index
     let (a, b) = strng.split_at(4);
     println!("{} - {}", b, a);
-    // // create an iterator from string using char as delimeter
-    // let mut str_count = strng.chars();
-    // let mut i = str_count.next();
-    // loop {
-    //     match i {
-    //         Some(x) => println!("{}", x),
-    //         None => break,
-    //     }
-    //     i = str_count.next();
-    // }
+    // create an iterator from string using char as delimeter
+    let mut str_count = strng.chars();
+    let mut i = str_count.next();
+    loop {
+        match i {
+            Some(x) => println!("{}", x),
+            None => break,
+        }
+        i = str_count.next();
+    }
 
     // let mut str_count = strng.split_whitespace();
     // let mut i = str_count.next();
